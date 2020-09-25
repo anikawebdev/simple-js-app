b016c2af41c3ae425aaab2f25a05ce13b1c0bd50
anikawebdev <anicus19@gmail.com>
2020-09-24 18:04:43 -0700

    added pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert("Hello World");
+let pokemonList = [];
+console.log(pokemonList);
 
-let favoriteFood = "clam chowder";
-document.write(favoriteFood);
+pokemonList = [
+  { name: "Jigglypuff", height: 0.5, types: ["cute charm", "friend guard"] },
+  { name: "Pikachu", height: 0.4, types: ["static", "lightning rod"] },
+  { name: "Shaymin", height: 0.2, types: ["natural cure"] },
+  {
+    name: "Clefairy",
+    height: 0.6,
+    types: ["cute charm", "magic guard", "friend guard"],
+  },
+  {
+    name: "Lillipup",
+    height: 0.4,
+    types: ["run away", "pick up", "vital spirit"],
+  },
+  {
+    name: "Togepi",
+    height: 0.3,
+    types: ["serene grace", "hustle", "super luck"],
+  },
+];
+
+console.log(pokemonList);
